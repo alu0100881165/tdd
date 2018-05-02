fa3cef03fa1025ce4571af810bd23d4bb15c92bf
alu0100881165 <ccpdanielnuezwehbe@gmail.com>
2018-05-02 11:00:41 +0100

    Cuarta prueba. Arreglado

--- tdd.js
+++ tdd.js
@@ -2,13 +2,29 @@ var assert = require('assert');
 
 function Raices(a, b, c)
 {
-  var Raiz_oper = Math.sqrt(b * b - 4 * a * c);
-  var R1 = (-b + Raiz_oper) / (2 * a);
-  var R2 = (-b - Raiz_oper) / (2 * a);
-  return [[R1,0],[R2,0]];
+  var Operacion = b * b - 4 * a * c;
+
+  if(Operacion > 0)
+  {
+    var Raiz_oper = Math.sqrt(Operacion);
+    var Real1 = (-b + Raiz_oper) / (2 * a);
+    var Real2 = (-b - Raiz_oper) / (2 * a);
+    var Imag1 = 0;
+    var Imag2 = 0;
+  }
+  else
+  {
+    var Raiz_oper = Math.sqrt(-Operacion);
+    var Real1 = 0;
+    var Real2 = -0;
+    var Imag1 = (-b + Raiz_oper) / (2 * a);
+    var Imag2 = (-b - Raiz_oper) / (2 * a);
+  }
+
+  return [[Real1,Imag1],[Real2,Imag2]];
 }
 
 assert.deepEqual(Raices(1,-5,0), [[5,0],[0,0]]);
 assert.deepEqual(Raices(2,5,0), [[0,0],[-2.5,0]]);
 assert.deepEqual(Raices(1,5,0), [[0,0],[-5,0]]);
-assert.deepEqual(Raices(1,0,1), [[0,2],[0,-2]]);
+assert.deepEqual(Raices(1,0,1), [[0,1],[0,-1]]);
